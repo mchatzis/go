39eb93e2a74dc479e26d1600f09edf1f1ab40d79
Michail Chatzis <mchatzis3@gmail.com>
2024-09-21 12:30:47 +0200

    Added logging implementation

---
+++ producer/pkg/logging/logger.go
@@ -0,0 +1,107 @@
+package logging
+
+import (
+	"io"
+	"log"
+	"os"
+	"sync"
+)
+
+type LogLevel int
+
+const (
+	DEBUG LogLevel = iota
+	INFO
+	WARN
+	ERROR
+)
+
+type logger struct {
+	debugLogger *log.Logger
+	infoLogger  *log.Logger
+	warnLogger  *log.Logger
+	errorLogger *log.Logger
+	level       LogLevel
+}
+
+var (
+	singleton *logger
+	once      sync.Once
+)
+
+func GetLogger() *logger {
+	once.Do(func() {
+		singleton = newLogger(os.Stdout, INFO)
+	})
+	return singleton
+}
+
+func newLogger(out io.Writer, level LogLevel) *logger {
+	return &logger{
+		debugLogger: log.New(out, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile),
+		infoLogger:  log.New(out, "INFO:  ", log.Ldate|log.Ltime|log.Lshortfile),
+		warnLogger:  log.New(out, "WARN:  ", log.Ldate|log.Ltime|log.Lshortfile),
+		errorLogger: log.New(out, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+		level:       level,
+	}
+}
+
+func SetLogLevel(level LogLevel) {
+	GetLogger().level = level
+}
+
+func SetOutput(out io.Writer) {
+	logger := GetLogger()
+	logger.debugLogger.SetOutput(out)
+	logger.infoLogger.SetOutput(out)
+	logger.warnLogger.SetOutput(out)
+	logger.errorLogger.SetOutput(out)
+}
+
+func (l *logger) Debug(v ...interface{}) {
+	if l.level <= DEBUG {
+		l.debugLogger.Println(v...)
+	}
+}
+
+func (l *logger) Debugf(format string, v ...interface{}) {
+	if l.level <= DEBUG {
+		l.debugLogger.Printf(format, v...)
+	}
+}
+
+func (l *logger) Info(v ...interface{}) {
+	if l.level <= INFO {
+		l.infoLogger.Println(v...)
+	}
+}
+
+func (l *logger) Infof(format string, v ...interface{}) {
+	if l.level <= INFO {
+		l.infoLogger.Printf(format, v...)
+	}
+}
+
+func (l *logger) Warn(v ...interface{}) {
+	if l.level <= WARN {
+		l.warnLogger.Println(v...)
+	}
+}
+
+func (l *logger) Warnf(format string, v ...interface{}) {
+	if l.level <= WARN {
+		l.warnLogger.Printf(format, v...)
+	}
+}
+
+func (l *logger) Error(v ...interface{}) {
+	if l.level <= ERROR {
+		l.errorLogger.Println(v...)
+	}
+}
+
+func (l *logger) Errorf(format string, v ...interface{}) {
+	if l.level <= ERROR {
+		l.errorLogger.Printf(format, v...)
+	}
+}
